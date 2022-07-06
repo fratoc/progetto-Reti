e2512b750cab69c252277624502720cfca178562
fratoc <toccafondifrancesco@gmail.com>
2022-07-06 12:40:47 +0200

    aggiunta sign in
    solo implementato button e accesso

--- script.js
+++ script.js
@@ -13,3 +13,13 @@ if (close) {
         nav.classList.remove('active');
     })
 }
+
+function onSignIn(googleUser) {
+    alert("in");
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+    alert('Ciao ' + profile.getName());
+  }
